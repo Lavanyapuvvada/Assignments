e2257952bbb2724532fc9ff1236ce1910a47baf5
Lavanya Puvvada <contactlavanypuvvada@gmail.com>
2025-01-27 16:36:36 +0530

    Assigment related to datatypes got added

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -4,7 +4,54 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		float temperature = 25.5f;
+		System.out.println("temparature is "+ temperature);
+		boolean customerOrder = true;
+		System.out.println("Customer Order is "+ customerOrder);
+		String phoneNumber = "123-456-7890l";
+		System.out.println("Phone Number is "+phoneNumber );
+		double amount = 1000.50;
+		System.out.println(" Amount is"+ amount);
+		String email = "john.doe@example.com";
+		System.out.println(" Email ID is"+ email);
+		float latitude = 37.7749f;
+		float longitude = -122.4194f;
+		System.out.println("latitude is "+ latitude);
+		System.out.println(" longitude is " + longitude);
+		boolean maritalStatus = false;
+		System.out.println("Marital Status is "+ maritalStatus);
+		String profession = "Software Engineer";
+		System.out.println("Profession is "+ profession);
+		String favColor = "Blue";
+		System.out.println(" My fav color is "+ favColor);
+		int year = 2023;
+		System.out.println(" Year is "+ year);
+		int followers = 100000;
+		System.out.println(" The number of followers is "+ followers);
+		double rating = 7.5;
+		System.out.println(" Rating of the movie is"+ rating);
+		char bloodGroup = 'A';
+		System.out.println("Blood Group is "+ bloodGroup);
+		String bookTitle = "To Kill a Mockingbird";
+		System.out.println("The title of the book is "+ bookTitle);
+		int employeesNumber = 50;
+		System.out.println(" The Number of employees is  "+ employeesNumber);
+		String countryName = "United States";
+		System.out.println(" Country Name is "+ countryName);
+		String eyeColor = "Brown";
+		System.out.println(" Eye Color is "+ eyeColor);
+		String birthplace = "New York City";
+		System.out.println(" Birth Place is "+ birthplace);
+		double distance = 200.5;
+		System.out.println(" Distance is "+ distance);
+		
+		
+		
+		
+		
 
 	}
 
 }
+
